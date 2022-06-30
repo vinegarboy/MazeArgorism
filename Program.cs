70ceb68316799082aeac868c63086bd8abfb3aeb
vinegarboy <necogusa398@gmail.com>
2022-06-30 16:01:32 +0900

    初期化処理を作成

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Internal;
+using System;
 
 namespace MazeArgorism
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        int[,] maps = new int[,];
+        int maze_width,maze_height;
+        static void Main(string[] args){
+            Console.WriteLine("Set Map Height.");
+            maze_height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Set Map Width.");
+            maze_width = int.Parse(Console.ReadLine());
+            maps = new int[maze_width,maze_height];
+            for(int _x =0;_x<maze_width;_x++){
+                for(int _y = 0;_y < maze_height;_y++){
+                    maps[_x,_y] = 0;
+                }
+            }
         }
     }
 }
