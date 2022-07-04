5f8bfbb403f4bb05731a6d68e17827d547898b17
vinegarboy <necogusa398@gmail.com>
2022-07-04 17:20:34 +0900

    壁伸ばし法微修正

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace MazeArgorism
     class Program
     {
         static void Main(string[] args){
-            PollFall ew = new PollFall();
+            ExtendWall ew = new ExtendWall();
             Console.WriteLine("Set Map Height.");
             int height = int.Parse(Console.ReadLine());
             Console.WriteLine("Set Map Width.");
@@ -151,12 +151,11 @@ namespace MazeArgorism
         int maze_width,maze_height,sx,sy;
         Random rd = new Random();
         bool finish = false;
-        private int t = 0;
 
         public void Initialize(int width,int height){
             maze_width = width;
             maze_height = height;
-            if((width%2 != 1 || height%2 != 1)||(width>4||height>4)){
+            if((width%2 != 1 || height%2 != 1)||(width<=4||height<=4)){
                 throw new Exception("The size of the maze must be odd and at least 5 blocks.");
             }
             maps = new int[maze_width,maze_height];
@@ -176,99 +175,134 @@ namespace MazeArgorism
         }
 
         public void MakeWall(){
-            sx = rd.Next(1,maze_width);
-            sy = rd.Next(1,maze_height);
+            sx = 2*rd.Next(1,(int)(maze_width-2)/2);
+            sy = 2*rd.Next(1,(int)(maze_height-2)/2);
             maps[sx,sy] = 1;
             while(!finish){
+                maps[sx,sy] = 1;
+                DebugDate();
                 rd = new Random();
                 switch(rd.Next(0,4)){//上下左右
                     case 0:
-                        if(sy+2 >= maze_height-1){
+                        if(maps[sx,sy-2] == 1){
+                            while((maps[sx,sy] == 1)&&!FinishCheck()){
+                                sx = 2*rd.Next(1,(int)(maze_width-2)/2);
+                                sy = 2*rd.Next(1,(int)(maze_height-2)/2);
+                            }
                             break;
                         }
-                        if(maps[sx,sy+2]==1){
-                            break;
-                        }
-                        t = 0;
-                        maps[sx,sy+1] = 1;
-                        maps[sx,sy+2] = 1;
-                        sy+=2;
-                        break;
-
-                    case 1:
-                        if(sy-2 <= 0){
-                            break;
-                        }
-                        if(maps[sx,sy-2]==1){
-                            break;
-                        }
-                        t=1;
                         maps[sx,sy-1] = 1;
                         maps[sx,sy-2] = 1;
                         sy-=2;
                         break;
 
+                    case 1:
+                        if(maps[sx,sy+2] == 1){
+                            while((maps[sx,sy] == 1)&&!FinishCheck()){
+                                sx = 2*rd.Next(1,(int)(maze_width-2)/2);
+                                sy = 2*rd.Next(1,(int)(maze_height-2)/2);
+                            }
+                            break;
+                        }
+                        maps[sx,sy+1] = 1;
+                        maps[sx,sy+2] = 1;
+                        sy+=2;
+                        break;
+
                     case 2:
-                        if(sx-2 <= 0){
+                        if(maps[sx-2,sy] == 1){
+                            while((maps[sx,sy] == 1)&&!FinishCheck()){
+                                sx = 2*rd.Next(1,(int)(maze_width-2)/2);
+                                sy = 2*rd.Next(1,(int)(maze_height-2)/2);
+                            }
                             break;
                         }
-                        if(maps[sx-2,sy]==1){
-                            break;
-                        }
-                        t=2;
                         maps[sx-1,sy] = 1;
                         maps[sx-2,sy] = 1;
                         sx-=2;
                         break;
 
                     case 3:
-                        if(sx+2 >= maze_width-1){
+                        if(maps[sx+2,sy] == 1){
+                            while((maps[sx,sy] == 1)&&!FinishCheck()){
+                                sx = 2*rd.Next(1,(int)(maze_width-2)/2);
+                                sy = 2*rd.Next(1,(int)(maze_height-2)/2);
+                            }
                             break;
                         }
-                        if(maps[sx+2,sy]==1){
-                            break;
-                        }
-                        t=3;
                         maps[sx+1,sy] = 1;
                         maps[sx+2,sy] = 1;
                         sx+=2;
                         break;
                 }
-                for(int _y = 0;_y<maze_height;_y++){
-                    for(int _x = 0;_x < maze_width;_x++){
-                        Console.Write(maps[_x,_y]);
+                if(TouchWall(sx,sy)){
+                    while((maps[sx,sy] == 1)&&!FinishCheck()){
+                        sx = 2*rd.Next(1,(int)(maze_width-2)/2);
+                        sy = 2*rd.Next(1,(int)(maze_height-2)/2);
                     }
-                    Console.Write("\n");
-                }
-                Console.Write("\n\n");
-                if(!CheckWall(sx,sy,t)){
-                    sx = rd.Next(1,maze_width);
-                    sy = rd.Next(1,maze_height);
                 }
                 if(FinishCheck()){
                     break;
+                }else if(!CanMakeCheck(sx,sy)){
+                    sx = 2*rd.Next(1,(int)(maze_width-2)/2);
+                    sy = 2*rd.Next(1,(int)(maze_height-2)/2);
                 }
             }
         }
 
-        public bool CheckWall(int _x,int _y,int forward_t){
-            bool ret = true;
-            if((maps[_x+1,_y] == 1&&t!=2)||(maps[_x-1,_y] == 1&&t!=3)||(maps[_x,_y+1] == 1&&t!=1)||(maps[_x,_y-1] == 1&&t!=0)){
-                ret = false;
+        private bool TouchWall(int _x,int _y){
+            if((_x==0||_x==maze_width-1)||(_y==0||_y==maze_height-1)){
+                return true;
+            }else{
+                return false;
             }
-            return ret;
+
+        }
+        private bool CanMakeCheck(int _x,int _y){
+            int count = 0;
+            if(maps[_x-1,_y] == 1){
+                count++;
+            }
+            if(maps[_x+1,_y] == 1){
+                count++;
+            }
+            if(maps[_x,_y-1] == 1){
+                count++;
+            }
+            if(maps[_x,_y+1] == 1){
+                count++;
+            }
+            if(count >= 2){
+                return false;
+            }else{
+                return true;
+            }
         }
 
-        public bool FinishCheck(){
+        private bool FinishCheck(){
             bool ret = true;
-            for(int _x= 0;_x<maze_width;_x++){
-                for(int _y = 0;_y < maze_height;_y++){
+            for(int _x= 2;_x<maze_width-1;_x+=2){
+                for(int _y = 2;_y < maze_height-1;_y+=2){
                     if(!((_x+2 >= maze_width||maps[_x+2,_y]==1)&&(_x-2 <= 0||maps[_x-2,_y]==1)&&(_y-2 <= 0||maps[_x,_y-2]==1)&&(_y+2 >= maze_height||maps[_x,_y+2]==1))){
                         ret = false;
                     }
                 }
             }
             return ret;
+        }
+
+        private void DebugDate(){
+            for(int _y = 0;_y<maze_height;_y++){
+                for(int _x = 0;_x < maze_width;_x++){
+                    if(maps[_x,_y]==0){
+                        Console.Write("□");
+                    }else{
+                        Console.Write("■");
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
         }
 
         public void WriteDate(){
